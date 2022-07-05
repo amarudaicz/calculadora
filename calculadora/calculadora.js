99663cf90dd885d0e40b303ec92f8179d579c763
Amaru <amarudaicz10@gmail.com>
2022-07-05 20:12:44 -0300

    ACT

---
+++ calculadora/calculadora.js
@@ -0,0 +1,10 @@
+require(`./sumar.js`)
+require(`./restar.js`)
+require(`./dividir.js`)
+require(`./multiplicar.js`)
+
+
+console.log(sumar(10,20))
+console.log(restar(10,20))
+console.log(dividir(10,2))
+console.log(multiplicar(10,20))
